98b55b08847fd64db6d860b5db8a9df91dd0fe1d
Александра <bzzz18@yandex.ru>
2023-05-12 17:19:08 +0300

    hello

---
+++ src/main/java/pro/sky/skyprospring1/SkyproSpring1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpring1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospring1/SkyproSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
