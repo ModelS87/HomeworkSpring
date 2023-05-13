efef64be95b540b1d0c79babf9c4e72c68b8c169
Александра <bzzz18@yandex.ru>
2023-05-13 17:37:14 +0300

    check

---
+++ src/main/java/pro/sky/skyprospring1/controller/StoreController.java
@@ -0,0 +1,29 @@
+package pro.sky.skyprospring1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.skyprospring1.model.Item;
+import pro.sky.skyprospring1.service.StoreService;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/order")
+public class StoreController {
+    private final StoreService storeService;
+    public StoreController(StoreService storeService){
+        this.storeService = storeService;
+    }
+    @GetMapping("/add")
+    public void add(@RequestParam Set<Integer> id) {
+        storeService.add(id);
+    }
+    @GetMapping("/get")
+    public List<Item> get(){
+        return storeService.get();
+    }
+
+}
